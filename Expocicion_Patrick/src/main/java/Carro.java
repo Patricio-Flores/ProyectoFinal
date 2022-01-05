01e360fe81b130b21d2695b91dbfe98e9af4225b
Patricio <Patricio@DESKTOP-QH7AA1C>
2022-01-04 19:10:36 -0500

    email_modificacion

--- Expocicion_Patrick/src/main/java/Carro.java
+++ Expocicion_Patrick/src/main/java/Carro.java
@@ -14,7 +14,8 @@ public class Carro {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+       // carro
+       
     }
     
 }
