298b14312a297187a0c55b512e992fd30fbf5556
Patricio <Patricio@DESKTOP-QH7AA1C>
2022-01-04 19:17:38 -0500

    Ultima Modificacion

--- Expocicion_Patrick/src/main/java/Atributos.java
+++ Expocicion_Patrick/src/main/java/Atributos.java
@@ -1,3 +1,6 @@
+
+import javax.swing.JOptionPane;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -10,4 +13,86 @@
  */
 public class Atributos {
     
+    private String propietario;
+    private double valorcomercial;
+    private String color;
+    private String placa;
+    private String matriculado;
+    private int aniofabricacion;
+
+    public String getPropietario() {
+        return propietario;
+    }
+
+    public void setPropietario(String propietario) {
+        this.propietario = propietario;
+    }
+
+    public double getValorcomercial() {
+        return valorcomercial;
+    }
+
+    public void setValorcomercial(double valorcomercial) {
+        this.valorcomercial = valorcomercial;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    public String getPlaca() {
+        return placa;
+    }
+
+    public void setPlaca(String placa) {
+        this.placa = placa;
+    }
+
+    public String getMatriculado() {
+        return matriculado;
+    }
+
+    public void setMatriculado(String matriculado) {
+        this.matriculado = matriculado;
+    }
+
+    public int getAniofabricacion() {
+        return aniofabricacion;
+    }
+
+    public void setAniofabricacion(int aniofabricacion) {
+        this.aniofabricacion = aniofabricacion;
+    }
+
+    public Atributos() {
+        JOptionPane.showInternalMessageDialog(null, "BIENVENIDOS LOS DATOS DEL VEHICULO SON ");
+    }
+
+    public String calcularAnio() {
+        double anioActual = 2021 - aniofabricacion;
+        if (anioActual >= 2) {
+            return "No debe de Presentarse a Revision ";
+
+        } else {
+            return "¡Alerta! \n, Si debe de`presentarse a la Revision";
+
+        }
+    }
+
+    public String datos() {
+        String informacion = "Informacion del Vehiculo \n"
+                + "Propietario: " + propietario + "\n"
+                + "Valor Comercial: " + valorcomercial + "\n"
+                + "Color: " + color + "\n"
+                + "Placa: " + placa + "\n"
+                + "Matriculado: " + matriculado + "\n"
+                + "Anio Fabricacion: " + aniofabricacion + "\n";
+
+        return informacion;
+    }
 }
+
--- Expocicion_Patrick/src/main/java/Carro.java
+++ Expocicion_Patrick/src/main/java/Carro.java
@@ -1,21 +1,31 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- *
- * @author Patricio
- */
+ import javax.swing.JOptionPane;
 public class Carro {
 
-    /**
-     * @param args the command line arguments
-     */
+  
+     
     public static void main(String[] args) {
        // carro
-       
+          
+        Atributos objeto = new Atributos();
+        String propietario = JOptionPane.showInputDialog("Introduce el Propietario ");
+
+        String valor = JOptionPane.showInputDialog("Introduce valor comercial");
+        int valorcomercial = Integer.parseInt(valor);
+        String color = JOptionPane.showInputDialog("Introduce el color ");
+        String placa = JOptionPane.showInputDialog("Introduce la Placa ");
+        String matriculado = JOptionPane.showInputDialog("Introduce si esta matriculado ");
+        String anio = JOptionPane.showInputDialog("Introduce Año de Matriculacion ");
+        int aniofabricacion = Integer.parseInt(anio);
+
+        objeto.setPropietario(propietario);
+        objeto.setValorcomercial(valorcomercial);
+        objeto.setColor(color);
+        objeto.setMatriculado(matriculado);
+        objeto.setAniofabricacion(aniofabricacion);
+
+        JOptionPane.showMessageDialog(null, objeto.datos() + "\n" + objeto.calcularAnio() + "\n");
+
+    }
     }
     
-}
+
